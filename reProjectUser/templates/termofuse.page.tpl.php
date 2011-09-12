21f4dfc71f777372b0e602b603d285cfa9a3c655
Shuyan Zeng <miyula1988@gmail.com>
2011-09-12 15:59:14 +0300

    backup

---
+++ reProjectUser/templates/termofuse.page.tpl.php
@@ -0,0 +1,13 @@
+<?php
+// $Id$
+/**
+ * Template to display term of use.
+ *
+ * Fields available:
+ * $project, the project object
+ */
+?>
+<div class='node-content-div'><?=$project->termofuse->body;?></div>
+<div class='join-button-div'>
+    <form><input type='hidden' value=''/><input type='image' src=''/></form>
+</div>
