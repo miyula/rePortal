7ad3bc1f6e1a5160b30f4e6ffb9010d74622b671
miyula1988@gmail.com <miyula@miyula-ThinkPad-T420.(none)>
2011-09-12 22:56:47 +0300

    add manage pages

---
+++ reProject/templates/manage.page.tpl.php
@@ -0,0 +1,26 @@
+<?php
+// $Id$
+/**
+ * Template to display project manage page.
+ *
+ * Fields available:
+ * $project, the project object
+ */
+?>
+<div class="project-manage-div">
+<div class="left-navigation-div">
+    <ul>
+        <li><a href="<?=url('project/'.$project->path)?>">Home page</a></li>
+        <li><a href="<?=url('project/'.$project->path.'/manage/tools')?>">Manage tools</a></li>
+        <li><a href="<?=url('project/'.$project->path.'/manage/contents')?>">Manage content</a></li>
+        <li>Manage persons</li>
+        <li>Send message</li>
+        <li>Settings</li>
+    </ul>
+</div>
+<div class="right-content-div">
+    <h1><?=$project->subtitle; ?></h1>
+    <?=$project->right_part; ?>
+</div>
+<div style="clear:both"></div>
+</div>
