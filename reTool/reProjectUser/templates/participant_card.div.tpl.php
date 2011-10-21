cd1d18e2ca4e2c7da96830f8a6b5b90134fd77a9
Shuyan Zeng <miyula1988@gmail.com>
2011-10-21 14:18:19 +0300

    backup

---
+++ reTool/reProjectUser/templates/participant_card.div.tpl.php
@@ -0,0 +1,29 @@
+<?php
+// $Id$
+/**
+ * Template to display participant info
+ *
+ * Fields available:
+ * $row
+ * $path
+ * $selected_tag
+ */
+?>
+<div class="participant-card-email-div">
+    <span class="title-span">Email</span><?=$row->email;?>
+</div>
+<div class="participant-card-phone-div">
+    <span class="title-span">Phone</span><?=$row->phone;?>
+</div>
+<div class="participant-card-tags-div">
+    <div class="title-div">Tags</div>
+    <?php foreach($row->tags_array as $tag): ?>
+    <div class='participant-tag<?=($tag==$selected_tag?' selected_tag':'');?>' ><a href='<?=url("project/$path/manage/persons/filter/$tag");?>'><?=$tag; ?></a></div>
+    <?php endforeach; ?>
+    <div style="clear:both"></div>
+</div>
+<div class="participant-card-notes-div">
+    <div class="title-div">Notes</div>
+    <div class="notes-content-div"><?=nl2br($row->notes);?></div>
+    <div style="clear:both"></div>
+</div>
