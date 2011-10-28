bc9773c559561a95644058dbea9a1a2b7c8fddbc
Shuyan Zeng <miyula1988@gmail.com>
2011-10-28 15:58:53 +0300

    invite researcher function

--- reProjectUser/templates/invite_join_project.page.tpl.php
+++ reProjectUser/templates/invite_join_project.page.tpl.php
@@ -16,10 +16,18 @@
         <?=$person->exiting_user_join_form; ?>
     <?php endif; ?>
     <?php if(isset($person->login_to_join_form) ): ?>
-        <?=$person->login_to_join_form; ?>
+        <div class="invite-user-form-div">
+            <h3>Already have an account?</h3>
+            <?=$person->login_to_join_form; ?>
+        </div>
     <?php endif; ?>
      <?php if(isset($person->register_to_join_form) ): ?>
-        <?=$person->register_to_join_form; ?>
+        <div class='middle-bar'></div>
+        <div class="invite-user-form-div">
+            <h3>Are you a new user?</h3>
+            <?=$person->register_to_join_form; ?>
+        </div>
     <?php endif; ?>
+    <div style='clear:both'></div>
     </div>
 </div>
