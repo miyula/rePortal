faa054338b685624b4b45a6bdae4683bf1bc2218
Shuyan Zeng <miyula1988@gmail.com>
2011-11-21 18:17:33 +0200

    backup

---
+++ reProject/templates/manage.home.page.tpl.php
@@ -0,0 +1,25 @@
+<?php
+
+// $Id$
+/**
+ * Template to display project manage home page.
+ *
+ * Fields available:
+ * $blocks, array
+ */
+?>
+<h3>Manage categories</h3>
+<div class="project-manage-categories-div">
+    <div class="project-manage-categorie-item-div">
+        <div class="item-left-div">
+            <image src=""/>
+        </div>
+        <div class="item-right-div">
+            <dl>
+                <dt></dt>
+                <dd></dd>
+            </dl>
+        </div>
+    </div>
+    <div style="clear:both"></div>
+</div>
