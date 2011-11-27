fc6f1692460546a377320e82e276241e545bafb9
Shuyan Zeng <miyula1988@gmail.com>
2011-11-27 19:45:25 +0200

    new change

--- reProjectUser/templates/invite_join_project.page.tpl.php
+++ reProjectUser/templates/invite_join_project.page.tpl.php
@@ -16,18 +16,10 @@
         <?=$person->exiting_user_join_form; ?>
     <?php endif; ?>
     <?php if(isset($person->login_to_join_form) ): ?>
-        <div class="invite-user-form-div">
-            <h3>Already have an account?</h3>
-            <?=$person->login_to_join_form; ?>
-        </div>
+        <?=$person->login_to_join_form; ?>
     <?php endif; ?>
      <?php if(isset($person->register_to_join_form) ): ?>
-        <div class='middle-bar'></div>
-        <div class="invite-user-form-div">
-            <h3>Are you a new user?</h3>
-            <?=$person->register_to_join_form; ?>
-        </div>
+        <?=$person->register_to_join_form; ?>
     <?php endif; ?>
-    <div style='clear:both'></div>
     </div>
 </div>
